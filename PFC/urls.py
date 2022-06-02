5d57a4c0801f43da0e1ac6686aa1c42ea9207dc9
Joan Camarotta <camarottajoan@gmail.com>
2022-06-02 17:02:50 -0300

    Agregados panel de registro/inicio y sus views.

--- PFC/urls.py
+++ PFC/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from PFC.views import index
+from PFC.views import index, registro, login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('index/', index),
     path('Entretenimiento/', include('blog.urls')),
+    path('registro/', registro),
+    path('login/', login),
 ]
--- PFC/views.py
+++ PFC/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def registro(request):
+    return render(request, 'registro.html')
+    
+def login(request):
+    return render(request, 'login.html')
