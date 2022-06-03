f1745d70431f24db16ad062a74d283400012f867
Pancho3110 <105683410+Pancho3110@users.noreply.github.com>
2022-06-03 11:16:23 -0300

    cambios

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from blog.models import Entretenimiento
+from blog.models import Salud
 # Register your models here.
 
 admin.site.register(Entretenimiento)
+admin.site.register(Salud)
